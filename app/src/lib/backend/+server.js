ceadc3ca2643a75ddf086419854c5f201502e6e6
Soumyadeep <111586127+saha-alpha9@users.noreply.github.com>
2023-08-15 21:23:58 +0530

    Update +server.js

--- app/src/lib/backend/+server.js
+++ app/src/lib/backend/+server.js
@@ -5,7 +5,7 @@ const { error } = require('winston');
 
 const app = express();
 const port = process.env.PORT || 5000;
-const MONGODB_URI = 'mongodb+srv://soumyadeepsaha2345:SSaha@1300709@cluster0.htoh2ql.mongodb.net/?retryWrites=true&w=majority';
+const MONGODB_URI = 'mongodb+srv://soumyadeepsaha2345:<password>@cluster0.htoh2ql.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
